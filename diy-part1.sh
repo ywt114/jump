f48fae5484ffda9ed8709a6278926f52dd760bbf
ywt114 <58467878+ywt114@users.noreply.github.com>
2022-10-18 19:47:17 +0800

    Create diy-part1.sh

---
+++ diy-part1.sh
@@ -0,0 +1,8 @@
+# 文件名: diy-part1.sh
+# 描述: OpenWrt DIY script part 1 (放在更新feeds之前)
+
+# 修改feeds源
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# 添加feeds源
+#echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
