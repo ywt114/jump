3450af57bd9370457028a50a96c1401c51847d26
ywt114 <58467878+ywt114@users.noreply.github.com>
2024-11-02 16:23:35 +0800

    Add files via upload

--- diy-part2_6.1.sh
+++ diy-part2_6.1.sh
@@ -126,6 +126,10 @@ git clone -b master https://github.com/linkease/nas-packages package/lean/nas-pa
 \cp -rf package/lean/nas-packages/network/services/* package/network/services
 \cp -rf package/lean/nas-packages/multimedia package
 \rm -rf package/lean/nas-packages
+sed -i 's/fw.koolcenter.com\/binary\/ddnsto/github.com\/ywt114\/diy\/releases\/download\/services/g' package/network/services/ddnsto/Makefile
+sed -i 's/fw.koolcenter.com\/binary\/iStoreEnhance/github.com\/ywt114\/diy\/releases\/download\/services/g' package/network/services/istoreenhance/Makefile
+sed -i 's/fw0.koolcenter.com\/binary\/LinkEase\/LinuxStorage/github.com\/ywt114\/diy\/releases\/download\/services/g' package/network/services/linkease/Makefile
+sed -i 's/fw0.koolcenter.com\/binary\/LinkEase\/AutoUpgrade\/linkmount/github.com\/ywt114\/diy\/releases\/download\/services/g' package/network/services/linkmount/Makefile
 
 # 创建automake-1.15软连接
 # sudo -E ln -sf /usr/bin/automake /usr/bin/automake-1.15
