2bc7208673f13f53756ef94443a6e788484b009c
ywt114 <58467878+ywt114@users.noreply.github.com>
2024-12-18 21:43:54 +0800

    Add files via upload

--- diy-part2_5.15.sh
+++ diy-part2_5.15.sh
@@ -92,6 +92,7 @@ git clone -b lede https://github.com/pymumu/luci-app-smartdns package/lean/luci-
 git clone -b master https://github.com/kenzok8/small package/lean/small
 # \rm -rf package/lean/small/luci-app-bypass package/lean/small/luci-app-homeproxy package/lean/small/luci-app-mihomo package/lean/small/luci-app-mosdns package/lean/small/luci-app-passwall2
 \rm -rf package/lean/small/{*bypass*,*homeproxy*,*mihomo*,*mosdns*,*passwall2*}
+sed -i 's/-flto/-flto -Wno-error/g' package/lean/small/shadowsocksr-libev/Makefile
 git clone -b main https://github.com/sirpdboy/luci-app-chatgpt-web package/lean/luci-app-chatgpt-web
 git clone -b master https://github.com/sirpdboy/luci-app-advanced package/lean/luci-app-advanced
 git clone -b master https://github.com/sirpdboy/luci-app-autotimeset package/lean/luci-app-autotimeset
